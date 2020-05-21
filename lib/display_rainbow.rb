19235e613506a2ac455d9024457514d2e2112f4e
Aqsa Pervaiz <aqsapervaiz@gmail.com>
2020-05-21 22:46:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ def display_rainbow (colors)
 end
 
 def display_rainbow (colors)
-  puts "R: #{1}, O: #{2}, Y: #{3}, G: #{4}, B: #{5}, I: #{6}, V: #{7}"
+  puts "R:#{red[0]},O:#{orange[1]},Y:#{yellow[2]},G:#{green[3]},B:#{blue[4]},I:#{indigo[5]},V:#{violet[6]}
 end
